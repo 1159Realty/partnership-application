9df664efc99f39cd867e99fd917ebe2a9f717bda
CEEPREL <ceeprel3@gmail.com>
2025-08-20 20:53:18 +0100

    Fix bug from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PROTECTED_ROUTES, ROUTES, UNPROTECTED_ROUTES, WEB_APP_URL } from "./utils/constants";
+import {
+  PROTECTED_ROUTES,
+  ROUTES,
+  UNPROTECTED_ROUTES,
+  WEB_APP_URL,
+} from "./utils/constants";
 import { getServerSession } from "./lib/session/server";
 import { getRole, getRoleRoutes } from "./lib/session/roles";
 
@@ -32,22 +37,35 @@ export default async function middleware(request: NextRequest) {
   const isSignedIn = Boolean(session);
 
   // Redirect unauthenticated users trying to access protected routes
-  if ((isProtectedRoute(currentPath) || isHomeRoute(request.nextUrl.pathname)) && !isSignedIn) {
-    return NextResponse.redirect(new URL(`${ROUTES["/sign-in"]}?redirect=${encodeURIComponent(fullPath)}`, request.url));
+  if (
+    (isProtectedRoute(currentPath) || isHomeRoute(request.nextUrl.pathname)) &&
+    !isSignedIn
+  ) {
+    return NextResponse.redirect(
+      new URL(
+        `${ROUTES["/sign-in"]}?redirect=${encodeURIComponent(fullPath)}`,
+        request.url
+      )
+    );
   }
 
   // TODO: Do not allow navigation to protected pages if onboarding is not complete!
 
   // Redirect signed-in users away from unprotected pages like /sign-in or /onboarding
-  if (isUnprotectedRoute(currentPath) && !isHomeRoute(request.nextUrl.pathname) && isSignedIn) {
+  if (
+    isUnprotectedRoute(currentPath) &&
+    !isHomeRoute(request.nextUrl.pathname) &&
+    isSignedIn
+  ) {
     return NextResponse.redirect(new URL(ROUTES["/"], request.url));
   }
 
   // Redirect signed-in users trying to access protected routes they don’t have access to
   const isTryingDisallowedRoute =
-    isProtectedRoute(currentPath) && allowedRoleRoutes.every((r) => !currentPath.includes(WEB_APP_URL + r));
+    isProtectedRoute(currentPath) &&
+    allowedRoleRoutes.every((r) => !currentPath.includes(WEB_APP_URL + r));
 
-  if (isTryingDisallowedRoute && !isHomeRoute(request.nextUrl.pathname) && isSignedIn) {
+  if (isTryingDisallowedRoute && !isHomeRoute(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL(ROUTES["/"], request.url));
   }
 }
